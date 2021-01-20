2c44636dfcc865dbb441db138b75645a6444ce91
Grigoriy Kharitonskiy <argh94@list.ru>
2021-01-21 01:17:48 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketprice = 42069;
+        int bonusmiles = ticketprice / 20;
+        System.out.println("Цена билета: " + ticketprice);
+        System.out.println("Бонусные мили: " + bonusmiles);
+    }
+}
