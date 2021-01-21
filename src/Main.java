c165d5869c475f65ff12c08714bf18147e714077
Grigoriy Kharitonskiy <argh94@list.ru>
2021-01-22 00:52:39 +0300

    Lower case variables

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int TicketPrice = 42069;
-        int BonusMiles = TicketPrice / 20;
-        System.out.println("Цена билета: " + TicketPrice);
-        System.out.println("Бонусные мили: " + BonusMiles);
+        int ticketPrice = 42069;
+        int bonusMiles = ticketPrice / 20;
+        System.out.println("Цена билета: " + ticketPrice);
+        System.out.println("Бонусные мили: " + bonusMiles);
     }
 }
